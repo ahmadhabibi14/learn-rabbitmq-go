fceedc28909145cd7a5042fa856cc8a13182d4a5
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-12-12 11:58:55 +0800

    [feat] new compose file

--- consumer/main.go
+++ consumer/main.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	conn, err := amqp091.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp091.Dial("amqp://habi:habi1234@localhost:5672/")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
-	channel, err := conn.Channel()
-	if err != nil {
-		panic(err)
-	}
-	
-	ctx := context.Background()
-	emailConsumer, err := channel.ConsumeWithContext(ctx,
-		"email", "consumer-email",
-		true, false, false, false, nil,
-	)
+	_, err = conn.Channel()
 	if err != nil {
 		panic(err)
 	}
 
-	for message := range emailConsumer {
-		fmt.Println("Routing key:", message.RoutingKey)
-		fmt.Println("Body:", string(message.Body))
-	}
+	fmt.Println(`connected to RabbitMQ`)
+	
+	// ctx := context.Background()
+	// emailConsumer, err := channel.ConsumeWithContext(ctx,
+	// 	"email", "consumer-email",
+	// 	true, false, false, false, nil,
+	// )
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for message := range emailConsumer {
+	// 	fmt.Println("Routing key:", message.RoutingKey)
+	// 	fmt.Println("Body:", string(message.Body))
+	// }
 }
